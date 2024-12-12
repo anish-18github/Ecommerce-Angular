ba3514c086c33dddf014f233fbd79afd13c25f54
anish-18github <anishgaikar17@gmail.com>
2024-12-12 22:02:13 +0530

    working on product endup getting error, igames are not getting displayed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MainCarouselComponent } from './components/home/main-carousel/main-carousel.component';
+import { HomeProductCardComponent } from './components/home/home-product-card/home-product-card.component';
+import { ProductSliderComponent } from './components/home/product-slider/product-slider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainCarouselComponent
+    MainCarouselComponent,
+    HomeProductCardComponent,
+    ProductSliderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { MainCarouselComponent } from "./main-carousel/main-carousel.component";
+import { menJeans } from '../../../Data/Men/Men_jeans';
+import { gounsPage1 } from '../../../Data/Gouns/gouns';
+import { lehngacholiPage2 } from '../../../Data/Saree/lenghaCholiPage2';
+import { kurtaPage1 } from '../../../Data/Kurta/kurta';
+import { mensShoesPage1 } from '../../../Data/shoes';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +12,17 @@ import { MainCarouselComponent } from "./main-carousel/main-carousel.component";
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  menJeans: any
+  womenGouns: any
+  lehangaCholi: any
+  mensKurta: any
+  mensShoes: any
 
+  ngOnInit() {
+    this.menJeans = menJeans.slice(0, 5)
+    this.womenGouns = gounsPage1.slice(0, 5)
+    this.lehangaCholi = lehngacholiPage2.slice(0, 5)
+    this.mensKurta = kurtaPage1.slice(0, 5)
+    this.mensShoes = mensShoesPage1.slice(0, 5)
+  }
 }
